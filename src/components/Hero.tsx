05fb6b185a6e0bb6906e446295617cd684036496
Shantanu <shantanusaini7000@gmail.com>
2024-08-20 18:38:41 +0530

    refactor hero section

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,17 +1,24 @@
-import { heroData } from "@/variables/pageData"
+import { heroData } from "@/variables/pageData";
+import Link from "next/link";
 
 function Hero() {
     return (
-        <div 
-        className="h-screen w-full bg-fixed flex flex-col justify-center items-center bg-slate-800" >
-            <h1 className="text-4xl font-bold mb-4">{heroData.main}</h1>
-            <p className="text-white">{heroData.sub}</p>
-            <button>{heroData.btn}</button>
+        <div
+            className="h-screen w-full bg-gradient-to-r from-indigo-600 via-purple-600 to-pink-600 flex flex-col justify-center items-center"
+        >
+            <h1 className="text-4xl font-bold text-white mb-4">{heroData.main}</h1>
+            <p className="text-white text-lg">{heroData.sub}</p>
+            <Link href='/login'>
+                <button className="mt-6 px-6 py-2 bg-white text-indigo-600 rounded-md shadow-lg hover:bg-gray-200 transition duration-300">
+                    {heroData.btn}
+                </button>
+            </Link>
         </div>
-    )
+    );
 }
 
-export default Hero
+export default Hero;
 
 
- // bg-[url('https://images.pexels.com/photos/2679323/pexels-photo-2679323.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1')]
+
+// bg-[url('https://images.pexels.com/photos/2679323/pexels-photo-2679323.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1')]
