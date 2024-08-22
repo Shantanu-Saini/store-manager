d15c00635ff167b2e9e96c8d37c17015b782ef5e
Shantanu <shantanusaini7000@gmail.com>
2024-08-22 15:17:27 +0530

    modifiy history section

--- src/app/profile/@history/page.tsx
+++ src/app/profile/@history/page.tsx
@@ -1,9 +1,23 @@
 "use client"
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 
 function HistoryPage() {
+  const [history, setHistory] = useState([]);
+
+  // useEffect(() => {
+
+  // }, [])
+
   return (
-    <div>History page</div>
+    <div className='space-y-4'>
+      <h1 className='text-center text-white text-3xl'>History</h1>
+      <div className="flex flex-col items-center justify-center h-screen bg-white rounded-md">
+        {
+          history.length <= 0 &&
+          <div className="text-center text-black text-lg">No history available</div>
+        }
+      </div>
+    </div>
   )
 }
 
--- src/app/profile/page.tsx
+++ src/app/profile/page.tsx
@@ -4,7 +4,7 @@ function ProfilePage() {
     console.log("HI from profile")
     return (
         <div>
-             <h2 className="text-2xl font-bold text-white text-center">Profile</h2>
+             <h2 className="text-center text-white text-3xl">Profile</h2>
         </div>
     );
 }
